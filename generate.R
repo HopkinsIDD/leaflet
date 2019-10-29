c3cb87ef0bbc7bfa00280512efa9fbc334c1b20a
epic.farmer <magicgathering2001@gmail.com>
2019-10-28 21:42:55 -0400

    Updated code to use variables for system specific filepaths

--- generate.R
+++ generate.R
@@ -1,45 +1,53 @@
-library(sf)
-library(leaflet)
-library(geojsonsf)
-library(geojsonio)
+# library(sf)
+# library(leaflet)
+# library(geojsonsf)
+# library(geojsonio)
 
-pal <- colorNumeric("viridis", NULL)
+library(raster)
+library(tiler)
 
-b <- st_read('~/rates.geojson')
-b <- a[1:100,]
+# 
+# pal <- colorNumeric("viridis", NULL)
+# 
+# b <- st_read('~/rates.geojson')
+# b <- a[1:100,]
+# 
+# leaflet(b) %>%
+#   addTiles() %>%
+#   addPolygons(
+#     color = ~pal(log10(rate))
+#   )
 
-leaflet(b) %>%
-  addTiles() %>%
-  addPolygons(
-    color = ~pal(log10(rate))
-  )
+raster_directory <- "~/svn/cholera-taxonomy/trunk/manuscripts/GAVI Impact Estimation/data/"
+repository_directory <- "~/git/leaflet"
+## This will generate temporary files in your home directory
 
-rates <- raster::raster("~/svn/cholera-taxonomy/trunk/manuscripts/GAVI Impact Estimation/data/afro_2010-2016_lambdas_raster_stack_upd.grd")
+rates <- raster::raster(paste(raster_directory,"afro_2010-2016_lambdas_raster_stack_upd.grd",sep='/'))
 rates <- taxdat::aggregate_raster_xlayers(rates,mean)
-raster::writeRaster(rates,'~/rates.tif',overwrite=TRUE)
-raster::writeRaster(log10(rates),'~/log10rates.tif',overwrite=TRUE)
+raster::writeRaster(rates,paste(repository_directory,'rates.tif',sep='/'),overwrite=TRUE)
+raster::writeRaster(log10(rates),paste(repository_directory,'log10rates.tif',sep='/'),overwrite=TRUE)
 
-cases <- raster::raster("~/svn/cholera-taxonomy/trunk/manuscripts/GAVI Impact Estimation/data/afro_2010-2016_cases_raster_stack_upd.grd")
+cases <- raster::raster(paste(raster_directory,"afro_2010-2016_cases_raster_stack_upd.grd",sep='/'))
 cases <- taxdat::aggregate_raster_xlayers(cases,mean)
-raster::writeRaster(cases,'~/cases.tif',overwrite=TRUE)
+raster::writeRaster(cases,paste(repository_directory,'cases.tif',sep='/'),overwrite=TRUE)
 
 
 tiler::tile(
-  '~/log10rates.tif',
-  "~/git/leaflet/log10rates/tiles",
+  paste(repository_directory,'log10rates.tif',sep='/'),
+  paste(repository_directory,'log10rates','tiles',sep='/'),
   "0-7",
   crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 )
 tiler::tile(
-  '~/rates.tif',
-  "~/git/leaflet/rates/tiles",
+  paste(repository_directory,'rates.tif',sep='/'),
+  paste(repository_directory,'rates','tiles',sep='/'),
   "0-7",
   crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 )
 
 tiler::tile(
-  '~/cases.tif',
-  "~/git/leaflet/cases/tiles",
+  paste(repository_directory,'cases.tif',sep='/'),
+  paste(repository_directory,'cases','tiles',sep='/'),
   "0-7",
   crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 )
